2ae03a4e946c82fe147327ff7f80dca280674556
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-07-19 04:42:12 +0200

    fixes and improvements in examples/signum-specializer.lisp

--- examples/signum-specializer.lisp
+++ examples/signum-specializer.lisp
@@ -1,40 +1,63 @@
-(in-package "SPECIALIZABLE")
+;;;; signum-specializer.lisp --- signum specializer examples.
+;;;;
+;;;; Copyright (C) 2013, 2014 Christophe Rhodes,
+;;;;
+;;;; Author: Christophe Rhodes <csr21@cantab.net>
 
-;;;; SIGNUM-SPECIALIZER example
-(defclass signum-specializer (extended-specializer)
-  ((signum :initarg :signum :reader %signum)))
-(defclass signum-generic-function (specializable-generic-function)
-  ()
-  (:metaclass sb-mop:funcallable-standard-class))
+(cl:defpackage #:signum-specializer.example
+  (:use
+   #:cl
+   #:specializable))
 
-(define-extended-specializer signum (gf signum)
-  (make-instance 'signum-specializer :signum signum))
-(defmethod sb-pcl:unparse-specializer-using-class
-    ((gf signum-generic-function) (specializer signum-specializer))
-  `(signum ,(%signum specializer)))
+(cl:in-package #:signum-specializer.example)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defclass signum-specializer (extended-specializer)
+    ((signum :initarg :signum :reader %signum)))
+
+  (defclass signum-generic-function (specializable-generic-function)
+    ()
+    (:metaclass sb-mop:funcallable-standard-class))
+
+  (define-extended-specializer signum (gf signum)
+    (declare (ignore gf))
+    (make-instance 'signum-specializer :signum signum))
+  (defmethod sb-pcl:unparse-specializer-using-class
+      ((gf signum-generic-function) (specializer signum-specializer))
+    `(signum ,(%signum specializer))))
+
 (defmethod sb-pcl::same-specializer-p
     ((s1 signum-specializer) (s2 signum-specializer))
   (= (%signum s1) (%signum s2)))
 
-(defmethod generalizer-equal-hash-key ((gf signum-generic-function) (g signum-specializer))
+(defmethod generalizer-equal-hash-key ((gf signum-generic-function)
+                                       (g signum-specializer))
   (%signum g))
-(defmethod generalizer-of-using-class ((gf signum-generic-function) arg)
-  (typecase arg
-    (real (make-instance 'signum-specializer :signum (signum arg)))
+(defmethod generalizer-of-using-class ((gf signum-generic-function)
+                                       object arg-position)
+  (typecase object
+    (real (make-instance 'signum-specializer :signum (signum object)))
     (t (call-next-method))))
 
-(defmethod sb-pcl:specializer-type-specifier ((proto-generic-function signum-generic-function)
-                                              (proto-method standard-method)
-                                              (specializer signum-specializer))
-  (case (%signum specializer)
-    (-1 '(real  *  (0)))
-    (0  '(real  0   0))
-    (1  '(real (0)  *))))
-(defmethod specializer-accepts-generalizer-p ((gf signum-generic-function) (specializer signum-specializer) (thing signum-specializer))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defmethod sb-pcl:specializer-type-specifier
+      ((proto-generic-function signum-generic-function)
+       (proto-method standard-method)
+       (specializer signum-specializer))
+    (case (%signum specializer)
+      (-1 '(real  *  (0)))
+      (0  '(real  0   0))
+      (1  '(real (0)  *)))))
+(defmethod specializer-accepts-generalizer-p
+    ((gf signum-generic-function)
+     (specializer signum-specializer)
+     (thing signum-specializer))
   (if (= (%signum specializer) (%signum thing))
       (values t t)
       (values nil t)))
-(defmethod specializer-accepts-generalizer-p ((gf signum-generic-function) (specializer sb-mop:specializer) (thing signum-specializer))
+(defmethod specializer-accepts-generalizer-p ((gf signum-generic-function)
+                                              (specializer sb-mop:specializer)
+                                              (thing signum-specializer))
   (specializer-accepts-generalizer-p gf specializer (class-of (%signum thing))))
 
 ;;; note: this method operates in full knowledge of the object, and so
@@ -43,46 +66,61 @@
   (and (realp obj)
        (= (signum obj) (%signum specializer))))
 
-(defmethod specializer< ((gf signum-generic-function) (s1 signum-specializer) (s2 signum-specializer) generalizer)
+(defmethod specializer< ((gf signum-generic-function)
+                         (s1 signum-specializer)
+                         (s2 signum-specializer) generalizer)
   (declare (ignore generalizer))
   (if (= (%signum s1) (%signum s2))
       '=
       '/=))
-(defmethod specializer< ((gf signum-generic-function) (s1 signum-specializer) (s2 class) generalizer)
-  (declare (ignore generalizer))
+(defmethod specializer< ((gf signum-generic-function)
+                         (s1 signum-specializer)
+                         (s2 class)
+                         generalizer)
   '<)
-(defmethod specializer< ((gf signum-generic-function) (s1 signum-specializer) (s2 sb-mop:eql-specializer) generalizer)
-  (declare (ignore generalizer))
+(defmethod specializer< ((gf signum-generic-function)
+                         (s1 signum-specializer)
+                         (s2 sb-mop:eql-specializer)
+                         generalizer)
   '>)
-(defmethod specializer< ((gf signum-generic-function) (s1 sb-mop:specializer) (s2 signum-specializer) generalizer)
+(defmethod specializer< ((gf signum-generic-function)
+                         (s1 sb-mop:specializer)
+                         (s2 signum-specializer)
+                         generalizer)
   (invert-specializer<-relation (specializer< gf s2 s1 generalizer)))
 ;;; note: the need for this method is tricky: we need to translate
 ;;; from generalizers that our specializers "know" about to those that
 ;;; ordinary generic functions and specializers might know about.
-(defmethod specializer< ((gf signum-generic-function) (s1 sb-mop:specializer) (s2 sb-mop:specializer) (generalizer signum-specializer))
+(defmethod specializer< ((gf signum-generic-function)
+                         (s1 sb-mop:specializer)
+                         (s2 sb-mop:specializer)
+                         (generalizer signum-specializer))
   (specializer< gf s1 s2 (class-of (%signum generalizer))))
+
 
 ;;; tests / examples
-(eval
- '(progn
-   (defgeneric fact (n) (:generic-function-class signum-generic-function))
-   (defmethod fact ((n (signum 0))) 1)
-   (defmethod fact ((n (signum 1))) (* n (fact (1- n))))
-   (assert (eql (fact 6) 720))
-   (assert (eql (fact 6.0) 720.0))
-   (defmethod no-applicable-method ((gf (eql #'fact)) &rest args)
-     'gotcha)
-   (assert (eql (fact -6) 'gotcha))))
 
-(eval
- '(progn
-   (defgeneric signum-class-specializers (x)
-     (:generic-function-class signum-generic-function)
-     (:method-combination list))
-   (defmethod signum-class-specializers list ((x float)) 'float)
-   (defmethod signum-class-specializers list ((x integer)) 'integer)
-   (defmethod signum-class-specializers list ((x (signum 1))) 1)
-   (assert (equal (signum-class-specializers 1.0) '(1 float)))
-   (assert (equal (signum-class-specializers 1) '(1 integer)))
-   (assert (equal (signum-class-specializers -1.0) '(float)))
-   (assert (equal (signum-class-specializers -1) '(integer)))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defgeneric fact (n)
+    (:generic-function-class signum-generic-function)))
+(defmethod fact ((n (signum 0))) 1)
+(defmethod fact ((n (signum 1))) (* n (fact (1- n))))
+(assert (eql (fact 6) 720))
+(assert (eql (fact 6.0) 720.0))
+(defmethod no-applicable-method ((gf (eql #'fact)) &rest args)
+  (declare (ignore args))
+  'gotcha)
+(assert (eql (fact -6) 'gotcha))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defgeneric signum-class-specializers (x)
+    (:generic-function-class signum-generic-function)
+    (:method-combination list)))
+
+(defmethod signum-class-specializers list ((x float)) 'float)
+(defmethod signum-class-specializers list ((x integer)) 'integer)
+(defmethod signum-class-specializers list ((x (signum 1))) 1)
+(assert (equal (signum-class-specializers 1.0) '(1 float)))
+(assert (equal (signum-class-specializers 1) '(1 integer)))
+(assert (equal (signum-class-specializers -1.0) '(float)))
+(assert (equal (signum-class-specializers -1) '(integer)))
