3ebc5211a02be5d1000cf0164038b349734a8761
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-08-07 12:54:57 +0200

    simplification in SB-PCL::EXPAND-DEFMETHOD

--- src/pcl-patch/make-method-lambda-using-specializers.lisp
+++ src/pcl-patch/make-method-lambda-using-specializers.lisp
@@ -25,49 +25,44 @@
     2. initargs for the method instance
     3. a (possibly modified) method lambda-list or nil"))
 
-(defun expand-defmethod (name
-                         proto-gf
-                         proto-method
-                         qualifiers
-                         lambda-list
-                         body
-                         env)
-  (multiple-value-bind (parameters unspecialized-lambda-list specializers)
-      (parse-specialized-lambda-list lambda-list)
-    (declare (ignore parameters))
-    (let ((*method-name* `(,name ,@qualifiers ,specializers))
-          (method-lambda `(lambda ,unspecialized-lambda-list ,@body)))
-      (multiple-value-bind (method-function-lambda initargs new-lambda-list)
-          (make-method-lambda-using-specializers
-           proto-gf proto-method qualifiers specializers method-lambda env)
-        (let ((initargs-form (make-method-initargs-form
-                              proto-gf proto-method method-function-lambda
-                              initargs env))
-              (specializers-form (make-method-specializers-form
-                                  proto-gf proto-method specializers env)))
-          `(progn
-             ;; Note: We could DECLAIM the ftype of the generic function
-             ;; here, since ANSI specifies that we create it if it does
-             ;; not exist. However, I chose not to, because I think it's
-             ;; more useful to support a style of programming where every
-             ;; generic function has an explicit DEFGENERIC and any typos
-             ;; in DEFMETHODs are warned about. Otherwise
-             ;;
-             ;;   (DEFGENERIC FOO-BAR-BLETCH (X))
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X HASH-TABLE)) ..)
-             ;;   (DEFMETHOD FOO-BRA-BLETCH ((X SIMPLE-VECTOR)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X VECTOR)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X ARRAY)) ..)
-             ;;   (DEFMETHOD FOO-BAR-BLETCH ((X LIST)) ..)
-             ;;
-             ;; compiles without raising an error and runs without
-             ;; raising an error (since SIMPLE-VECTOR cases fall through
-             ;; to VECTOR) but still doesn't do what was intended. I hate
-             ;; that kind of bug (code which silently gives the wrong
-             ;; answer), so we don't do a DECLAIM here. -- WHN 20000229
-             ,(make-defmethod-form name qualifiers specializers-form
-                                   (or new-lambda-list unspecialized-lambda-list)
-                                   (if proto-method
-                                       (class-name (class-of proto-method))
-                                       'standard-method)
-                                   initargs-form)))))))
+(defun expand-defmethod (name proto-gf proto-method qualifiers lambda-list
+                         body env)
+  (binding* (((nil unspecialized-lambda-list specializers)
+              (parse-specialized-lambda-list lambda-list))
+             (*method-name* `(,name ,@qualifiers ,specializers))
+             (method-lambda `(lambda ,unspecialized-lambda-list ,@body))
+             ((method-function-lambda initargs new-lambda-list)
+              (make-method-lambda-using-specializers
+               proto-gf proto-method qualifiers specializers method-lambda env))
+             (initargs-form
+              (make-method-initargs-form
+               proto-gf proto-method method-function-lambda initargs env))
+             (specializers-form
+              (make-method-specializers-form
+               proto-gf proto-method specializers env)))
+    `(progn
+       ;; Note: We could DECLAIM the ftype of the generic function
+       ;; here, since ANSI specifies that we create it if it does not
+       ;; exist. However, I chose not to, because I think it's more
+       ;; useful to support a style of programming where every generic
+       ;; function has an explicit DEFGENERIC and any typos in
+       ;; DEFMETHODs are warned about. Otherwise
+       ;;
+       ;;   (DEFGENERIC FOO-BAR-BLETCH (X))
+       ;;   (DEFMETHOD FOO-BAR-BLETCH ((X HASH-TABLE)) ..)
+       ;;   (DEFMETHOD FOO-BRA-BLETCH ((X SIMPLE-VECTOR)) ..)
+       ;;   (DEFMETHOD FOO-BAR-BLETCH ((X VECTOR)) ..)
+       ;;   (DEFMETHOD FOO-BAR-BLETCH ((X ARRAY)) ..)
+       ;;   (DEFMETHOD FOO-BAR-BLETCH ((X LIST)) ..)
+       ;;
+       ;; compiles without raising an error and runs without raising
+       ;; an error (since SIMPLE-VECTOR cases fall through to VECTOR)
+       ;; but still doesn't do what was intended. I hate that kind of
+       ;; bug (code which silently gives the wrong answer), so we
+       ;; don't do a DECLAIM here. -- WHN 20000229
+       ,(make-defmethod-form name qualifiers specializers-form
+                             (or new-lambda-list unspecialized-lambda-list)
+                             (if proto-method
+                                 (class-name (class-of proto-method))
+                                 'standard-method)
+                             initargs-form))))
